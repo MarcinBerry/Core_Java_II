bbe1798d93912ffc71a9df56bde1da7767e1cd06
marcin <berneckimarcin.priv@gmail.com>
2020-11-25 19:40:37 +0100

    Page 47

--- src/main/java/collecting/CollectingIntoMaps.java
+++ src/main/java/collecting/CollectingIntoMaps.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CollectingIntoMaps {
+
 public static class Person {
     private int id;
     private String name;
